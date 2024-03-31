974e31cfbba5fadb7df1ce2ec93fc9f6cbb42b69
halfblood <arashyadegari0402@gmail.com>
2024-04-01 03:11:44 +0330

    change saving location

--- sub_downloader/runner.py
+++ sub_downloader/runner.py
@@ -7,17 +7,19 @@ desired language
 """
  
 import sys
+import os
 from subtitle_gen import SubtitleGenerator
 from googletrans import Translator
  
  
 def run():
+    path = os.getcwd()
     args = sys.argv[1:]
     video_id = args[0]
     source_lang = args[1]
     dest_lang = args[2]
     translator = Translator()
     sg = SubtitleGenerator(video_id=video_id, src_lang=source_lang, dst_lang=dest_lang, translator=translator)
-    sg.save_subtitle(filename="sub", keep_orginal=True)
+    sg.save_subtitle(filename=path +  "/" + "sub", keep_orginal=True)
  
 run()
